47ee5ca9719f6d307de282d56b5cf183b4af5399
hamzasaidi <hsaidi291@gmail.com>
2021-11-01 20:22:54 +0100

    Loading data finished

--- Tail/ViewModels/NewsFeedViewModel.cs
+++ Tail/ViewModels/NewsFeedViewModel.cs
@@ -15,46 +15,48 @@ namespace Tail.ViewModels
 {
     public class NewsFeedViewModel : PageViewModelBase
     {
-        int offset;
-        int limit;
+        int todayOffset = 0;
+        int dayBeforeOffset = 0;
+        int limit = 6;
         DateTimeOffset currentDate;
         HttpClient httpclient;
-        public ObservableRangeCollection<Data> _togdayNews;
-        public ObservableRangeCollection<Data> TodayNews { get { return _togdayNews; } set { SetProperty(ref _togdayNews, value); } }
-
-        public Data _selectedItem;
-        public Data SelectedItem { get { return _selectedItem; } set { SetProperty(ref _selectedItem, value); } }
-
+        public ObservableRangeCollection<Data> _todayNews;
+        public ObservableRangeCollection<Data> TodayNews { get { return _todayNews; } set { SetProperty(ref _todayNews, value); } }
+        public ObservableRangeCollection<Data> _dayBeforeNews;
+        public ObservableRangeCollection<Data> DayBeforeNews { get { return _dayBeforeNews; } set { SetProperty(ref _dayBeforeNews, value); } }
         public bool _isInitialLoading;
         public bool IsInitialLoading { get { return _isInitialLoading; } set { SetProperty(ref _isInitialLoading, value); } }
-
         public ICommand LoadMoreCommand { get; set; }
+        public ICommand PositionChangedCommand { get; set; }
         public ICommand SelectedCommand { get; set; }
         public NewsFeedViewModel()
         {
             httpclient = new HttpClient();
-            offset = 0;
-            limit = 6;
             currentDate = DateTimeOffset.Now;
-            LoadMoreCommand = new Command(async () => await LoadMore());
-            SelectedCommand = new Command(async () => await ExecuteSelectedItem(SelectedItem));
+            LoadMoreCommand = new Command(async (list) => await LoadMore());
+            SelectedCommand = new Command<Data>(async (data) => await ExecuteSelectedItem(data));
             TodayNews = new ObservableRangeCollection<Data>();
-
-
+            DayBeforeNews = new ObservableRangeCollection<Data>();
+            PositionChangedCommand = new Command<int>(async (position) => await ExecutePositionChanged(position));
         }
 
-        private async Task ExecuteSelectedItem(Data selectedItem)
+        private async Task ExecutePositionChanged(int position)
+        {
+            if (position == TodayNews.Count - 1)
+            {
+                todayOffset += 1;
+                var response = await GetMediaStack(todayOffset, limit, DateTimeOffset.Now);
+                TodayNews.AddRange(response.ListData);
+            }
+        }
+        private async Task ExecuteSelectedItem(Data data)
         {
             try
             {
-                await Browser.OpenAsync(selectedItem.Url, BrowserLaunchMode.SystemPreferred);
+                await Browser.OpenAsync(data.Url, BrowserLaunchMode.SystemPreferred);
             }
-            catch (Exception ex)
-            {
-                // An unexpected error occured. No browser may be installed on the device.
-            }
+            catch (Exception ex) { }
         }
-
         public override async void OnPageAppearing()
         {
             base.OnPageAppearing();
@@ -66,21 +68,22 @@ namespace Tail.ViewModels
             }
 
         }
-
         private async Task InitNewsFeed()
         {
-            var response = await GetMediaStack(offset, limit, DateTimeOffset.Now);
-            TodayNews = new ObservableRangeCollection<Data>(response.ListData);
+            var TodayResponse = await GetMediaStack(todayOffset, limit, DateTimeOffset.Now);
+            TodayNews = new ObservableRangeCollection<Data>(TodayResponse.ListData);
+            var DayBeforeResponse = await GetMediaStack(dayBeforeOffset, limit, DateTimeOffset.Now.AddDays(-1));
+            DayBeforeNews = new ObservableRangeCollection<Data>(DayBeforeResponse.ListData);
         }
-
         private async Task LoadMore()
         {
-            var response = await GetMediaStack(offset + 1, limit, DateTimeOffset.Now);
-            TodayNews.AddRange(response.ListData);
+            dayBeforeOffset += 1;
+            var response = await GetMediaStack(dayBeforeOffset, limit, DateTimeOffset.Now);
+            DayBeforeNews.AddRange(response.ListData);
         }
         async Task<MediaStackResponse> GetMediaStack(int offset, int limit, DateTimeOffset dateTimeOffset)
         {
-            var url = new Uri(String.Format("{0}?access_key={1}&offset={2}&limit={3}&date={4}&categories=sports", Constants.MediaStackBaseUrl, Constants.MediaStackApiKey, offset, limit, dateTimeOffset.ToString("yyyy-MM-dd")));
+            var url = new Uri(String.Format("{0}?access_key={1}&offset={2}&limit={3}&date={4}&categories=sports&countries=us", Constants.MediaStackBaseUrl, Constants.MediaStackApiKey, offset, limit, dateTimeOffset.ToString("yyyy-MM-dd")));
             HttpResponseMessage res = await httpclient.GetAsync(url);
             if (res.IsSuccessStatusCode)
             {
@@ -90,11 +93,11 @@ namespace Tail.ViewModels
                 }
                 catch (Exception e)
                 {
-                    return null;
+                    return new MediaStackResponse();
                 }
 
             }
-            return null;
+            return new MediaStackResponse();
         }
 
 
